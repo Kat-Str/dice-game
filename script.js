3cc22bf84f08d02822b94f5eb1cf55ba08e3c9ab
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2024-01-08 17:28:27 +0100

    Added visual and sound effects

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var button = document.querySelector("#button");
 var winner = document.querySelector("#winner");
+
 var firstDice = document.querySelector("#first-players-dice");
 var secondDice = document.querySelector("#second-players-dice");
+
+var playerOne = document.querySelector("#playerOne");
+var playerTwo = document.querySelector("#playerTwo");
+
+var audio = document.querySelector("#audio");
 
 var randomNumberOne = "";
 var randomNumberTwo = "";
 
 function generateRandomNumbers() {
+  audio.play();
   randomNumberOne = Math.floor(Math.random() * 6) + 1;
   randomNumberTwo = Math.floor(Math.random() * 6) + 1;
   firstDice.setAttribute("src", `./media/dice${randomNumberOne}.png`);
@@ -15,10 +22,16 @@ function generateRandomNumbers() {
 }
 function announceWinner() {
   if (randomNumberOne > randomNumberTwo) {
+    playerOne.innerHTML = "Player 1 🏆";
+    playerTwo.innerHTML = "Player 2";
     winner.innerHTML = "Player 1 wins!";
   } else if (randomNumberOne < randomNumberTwo) {
+    playerTwo.innerHTML = "Player 2 🏆";
+    playerOne.innerHTML = "Player 1";
     winner.innerHTML = "Player 2 wins!";
   } else {
+    playerOne.innerHTML = "Player 1";
+    playerTwo.innerHTML = "Player 2";
     winner.innerHTML = "Draw!";
   }
 }
